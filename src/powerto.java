cbbfba0ebe2760410fea2aff582272d172cf2276
kadeve <kadeve@gmail.com>
2020-10-08 23:47:12 +0200

    power

---
+++ src/powerto.java
@@ -0,0 +1,9 @@
+public class powerto {
+    public static void  main(String[] args) {
+        int a=8,b=7,sum=a;
+        for(int i=1;i<b;i++){
+            sum *= a;
+        }
+        System.out.println(sum);
+    }
+}
